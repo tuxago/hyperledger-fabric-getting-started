56c474ce59e5c0cd9605b6eb95bd54a67edb32cf
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2017-06-13 15:07:59 +0000

    Use the new URL in the nexus for hyperledger

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 export ARCH=$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(go env GOARCH)
+export ARCH=linux-amd64
 
-curl https://nexus.hyperledger.org/content/repositories/logs/sandbox/fabric-binary/${ARCH}-1.0.0-alpha2.tar.gz | tar xz
+curl https://nexus.hyperledger.org/service/local/repositories/releases/content/org/hyperledger/fabric/fabric-binary/${ARCH}-1.0.0-alpha2/fabric-binary-${ARCH}-1.0.0-alpha2.tar.gz | tar xz
 cd release/${ARCH}
 sh download-dockerimages.sh -c $(uname -m)-1.0.0-alpha2 -f $(uname -m)-1.0.0-alpha2
 docker images
